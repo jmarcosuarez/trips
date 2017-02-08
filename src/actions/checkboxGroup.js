cf117a7202f937e32ca3bc003f6352e6ef24bda7
Marco Suarez <jmarcosuarez@gmail.com>
2017-02-08 13:26:44 +0000

    CheckboxGroup now saver arrays to filter store

--- src/actions/checkboxGroup.js
+++ src/actions/checkboxGroup.js
@@ -1,3 +1,4 @@
+import { List } from 'immutable';
 import * as actionTypes from '../constants/actionTypes';
 import { setFilterObject } from '../actions/filter';
 
@@ -10,24 +11,39 @@ export function toggle(checkboxName, checkboxGroupName) {
 }
 
 /**
-  * Helper fucntion:
+  * Helper function:
   * For saving filtering data we need the key of this specific item on the filters store
-  * also we need the new value to be saved (checking its store for the current value
-  (I presume saving was faster than this))
+  * also we need the new value to be saved (checking its store for the current value)
   */
 const prepareAndSendToFilter = (checkboxName, checkboxGroupName) => (dispatch, getState) => {
   const capitalized = checkboxGroupName.charAt(0).toUpperCase() + checkboxGroupName.slice(1);
+  // This key is the position where the checkbox item is sat at the filters store,
+  // we need it to save to here later on
   const key = getState().filter.get('filters').findIndex(listing => {
     return listing.get('id') === capitalized;
   });
-
+  // Also we need the first value at this keys position (If any)
+  const currentFilterValue = getState().filter.getIn(['filters', key, 'value', 0]);
+  const currentFilter = getState().filter.getIn(['filters', key, 'value']);
   // Here to retreive the current value of the checkbox selected we need the index where its at
-  const checkboxGroupKey = getState().checkboxGroup.get(checkboxGroupName).findIndex(listing => {
+  // Also, this is the value to be saved on the aaray in filters
+  const checkboxNameKey = getState().checkboxGroup.get(checkboxGroupName).findIndex(listing => {
     return listing.get('storeId') === checkboxName;
   });
-  // Then just retreive value the immutable way!
-  const value = getState().checkboxGroup.getIn([checkboxGroupName, checkboxGroupKey, 'initial']);
-  dispatch(setFilterObject(key, value));
+  // Then just retreive the current value of the checkbox, if "true" we add and if it is "false" we delete
+  const value = getState().checkboxGroup.getIn([checkboxGroupName, checkboxNameKey, 'initial']);
+
+  // We are saving keys of checkboxes selected and saving it as array
+  // in the filter position that corresponds based on checkboxGroupName
+  if (currentFilterValue !== undefined) {
+    const arr = value
+      ? currentFilter.push(checkboxNameKey)
+      : currentFilter.filter(item => item !== checkboxNameKey);
+    dispatch(setFilterObject(key, arr));
+  } else {
+    const arr = List([checkboxNameKey]);
+    dispatch(setFilterObject(key, arr));
+  }
 };
 
 
